9031ac7e1b84dd709ca170af77350500a36bcf88
fusillicode <fusillicode@gmail.com>
2016-06-12 02:43:38 +0200

    tired

--- app/models/light_source.rb
+++ app/models/light_source.rb
@@ -1,3 +1,28 @@
 class LightSource < ActiveRecord::Base
+  before_save :set_random_coordinates_if_missing
+
   has_many :sensors_data, class_name: 'SensorDatum'
+
+  private
+
+  def set_random_coordinates_if_missing
+    return if self.raw_data[:position].present?
+    self.raw_data = raw_data.merge random_position
+  end
+
+  def random_position
+    { position: random_coordinates }
+  end
+
+  def random_coordinates
+    { lat: random_lat, lng: random_lng }
+  end
+
+  def random_lat
+    rand(45.396454...45.541639)
+  end
+
+  def random_lng
+    rand(8.886936...9.314372)
+  end
 end
--- app/models/sensor_datum.rb
+++ app/models/sensor_datum.rb
@@ -3,27 +3,4 @@ class SensorDatum < ActiveRecord::Base
 
   self.table_name = "sensors_data"
   belongs_to :light_source
-
-  private
-
-  def set_random_coordinates_if_missing
-    return if self.raw_data[:position].present?
-    self.raw_data = raw_data.merge random_position
-  end
-
-  def random_position
-    { position: random_coordinates }
-  end
-
-  def random_coordinates
-    { lat: random_lat, lng: random_lng }
-  end
-
-  def random_lat
-    rand(45.396454...45.541639)
-  end
-
-  def random_lng
-    rand(8.886936...9.314372)
-  end
 end
